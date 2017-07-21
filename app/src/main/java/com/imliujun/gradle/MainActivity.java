229d2dd4cc0399401c8f99dfb732fa0bbf9d2ed2
zhouxingrong <zhouxingrong@putao.com>
2017-07-21 17:17:09 +0800

    fix

--- app/src/main/java/com/imliujun/gradle/MainActivity.java
+++ app/src/main/java/com/imliujun/gradle/MainActivity.java
@@ -3,6 +3,9 @@ package com.imliujun.gradle;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TextView;
+
+import com.imliujun.gradle.utils.AppUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //可以去 BuildConfig 中查看Gradle中定义的常量
         Log.i("tag", BuildConfig.DOMAIN_NAME);
+
+
+        TextView tv_version = (TextView) findViewById(R.id.tv_version);
+
+
+        String versionName = AppUtils.getVersionName(this);
+        tv_version.setText(versionName);
     }
 }
